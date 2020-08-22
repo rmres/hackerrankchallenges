1639178ea62a49944322cd2e62f6a81efc3f123f
rmres <robertmoraes1112@gmail.com>
2020-08-22 16:41:17 -0300

    Reversing elements(array)

---
+++ 30daysofcode/day7.cs
@@ -0,0 +1,30 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+
+        Array.Reverse(arr);
+        for (int i = 0; i < arr.Length; i++) {
+            Console.Write("{0} ", arr[i]);
+        }
+    }
+}
