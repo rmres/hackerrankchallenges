7f919262aa531103f26ea0e4273384624a3229ae
rmres <robertmoraes1112@gmail.com>
2020-08-23 01:42:39 -0300

    Compare the triplets

---
+++ comparethetriplets.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<int> compareTriplets(List<int> a, List<int> b) {
+        int[] arr = new int[2];
+        for (int i = 0; i < 3; i++) {
+            if (a[i] > b[i]) {
+                arr[0] += 1;
+            }
+            else if (a[i] < b[i]) {
+                arr[1] += 1;
+            }
+        }
+        return arr.OfType<int>().ToList();
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
