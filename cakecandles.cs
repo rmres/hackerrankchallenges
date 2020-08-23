7b7c9d3d5ce3fb36868b54a61beb8789877fdcb4
rmres <robertmoraes1112@gmail.com>
2020-08-23 20:03:55 -0300

    Birthday Cake Candles

---
+++ cakecandles.cs
@@ -0,0 +1,44 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+        Array.Sort(ar);
+        int count = 0;
+        int upper = ar[ar.GetUpperBound(0)];
+        for (int i = 0; i < ar.Length; i++) {
+            if (ar[i] == upper) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = birthdayCakeCandles(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
